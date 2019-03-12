707c40d5f8456ff768af104a39491eb9dbda47ef
JASON JOHNSON <JASJOHN3@uat.edu>
2019-03-12 11:29:15 -0700

    Code Modification for collisions

--- Hash_Table/Hash_Tableh.h
+++ Hash_Table/Hash_Tableh.h
@@ -2,17 +2,20 @@
 #ifndef HASH_H
 #define HASH_H
 #include <iostream>
-
+#include <thread>
+#include <string>
 template <typename K, typename V>
 struct Hash_Node
 {
 	V value;
 	K key;
+	Hash_Node<K, V>*next;
 
 	Hash_Node(K key, V value)
 	{
 		this->key = key;
 		this->value = value;
+		this->next = nullptr;
 	}
 };
 template <typename K, typename V>
@@ -21,7 +24,7 @@ class Hash_Table
 private:
 	Hash_Node<K, V> **Table;
 	//sets the initial size of the Hash Map
-	int Table_Size;
+	int Table_Size = 5;
 	//stores the current size
 	int size;
 
@@ -30,13 +33,44 @@ public:
 	Hash_Table()
 	{
 		Table = new Hash_Node<K, V>*[Table_Size];
+		for (int i = 0; i < Table_Size; ++i)
+		{
+			Table[i] = NULL;
+		}
+
 	}
 	void insert(const K & key, const V & value)
 	{
 		Hash_Node<K, V> * temp = new Hash_Node<K, V>(key, value);
 
 		//apply hash function to the index of the inserting key and value pair
-		int hash_index = hash_code(key);
+		std::hash<K> string_hash;
+		int hash_index = string_hash(key);
+		hash_index = hash_index % Table_Size;
+
+
+		if (Table[hash_index] == NULL)
+		{
+			Table[hash_index] = temp;
+		}
+		else { Table[hash_index] = temp->next; }
+
+
+		current_node = Table[hash_index]
+			while (current_node != NULL)
+			{
+				// if matches overwrite
+				// if doesn't match then iterate and not null
+				// if at end then store it.
+				if (current_node->key )
+				{
+					Table[hash_index] = temp;
+				}
+				else
+				{
+					current_node = temp->next;
+				}
+			}
 
 	}
 	~Hash_Table()
