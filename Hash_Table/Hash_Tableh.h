0436f7708783a163828d876b07ff608e7a42390e
JasJohn3 <33557960+JasJohn3@users.noreply.github.com>
2019-03-16 17:12:41 -0700

    Completed Assignment

--- Hash_Table/Hash_Tableh.h
+++ Hash_Table/Hash_Tableh.h
@@ -11,12 +11,14 @@ struct Hash_Node
 	V value;
 	K key;
 	Hash_Node<K, V>*next;
+	Hash_Node<K, V>*previous;
 
 	Hash_Node(K key, V value)
 	{
 		this->key = key;
 		this->value = value;
 		this->next = nullptr;
+		this->previous = nullptr;
 	}
 };
 template <typename K, typename V>
@@ -56,81 +58,149 @@ public:
 	void insert(K  key, V value)
 	{
 
-		//apply hash function to the index of the inserting key and value pair
-		/*	temporary test function for collision testing
-			int hash_index = key % Table_Size;				*/
-
-		
-		/*	This is the real hash function for the table.
-			std::hash<K> hash_function;
-			int hash_index = hash_function(key);
-			hash_index = hash_index % Table_Size;			*/
-
 		std::hash<K> hash_function;
 		int hash_index = hash_function(key);
 		//added absolute value.  Hash function can return negative values.
 		hash_index = std::abs(hash_index % Table_Size);
+		//set a temporary node to our table value
 		Hash_Node<K, V>* current = Table[hash_index];
 		if (current == nullptr)
 		{
+			//if the node stored at the tables index value is null, we insert a new node with the key and value pair
 			Table[hash_index] = new Hash_Node<K, V>(key, value);
+			std::cout << "Insert new head Successful Key: " << key << " Value: " << value << std::endl;
 		}
 		else {
-
+			//if a node exists in our table, we iterate through the linked list comparing key values
 			while (current != nullptr)
 			{
+				//if the current nodes key value is the same as the key value inserted into our function, we overwrite the value stored in that node
 				if (current->key == key)
 				{
+					
 					current->value = value;
+					std::cout << "Insert Value Successful Key: " << current->key << " Value: " << current->value << std::endl;
 					break;
 				}
+				//if the current nodes next value is a null pointer, we create a new node at our current->next pointer to a new Node with the key value pair inserted into our function
 				else if (current->next == nullptr)
 				{
+					
 					current->next = new Hash_Node<K, V>(key, value);
+					Hash_Node<K, V>* temp = current->next;
+					temp->previous = current;
+					std::cout << "Insert Value Successful Key: " << current->key << " Value: " << current->value << std::endl;
 				}
+				//iterate to the next node in the list.
 				current = current->next;
-
 			}
 		}
 	}
-
-		
-
-
-	V find(K key)
+	void Remove(K key)
 	{
-
-		/*int hash_index = key % Table_Size;*/
-		
 		std::hash<K> hash_function;
+		//declaring an integer value to store the hash value
 		int hash_index = hash_function(key);
+		//Modding the hash value by the table size to get a remainder for our index value
 		hash_index = std::abs(hash_index % Table_Size);
-		Hash_Node<K,V>* current = Table[hash_index];
+		//creating a temporary NODE to be able to iterate through our linked list values
+		Hash_Node<K,V> * Delete_Node = Table[hash_index];
 		
+		while (Delete_Node!= NULL)
+		{
+			//case 1 Top of list
+			if (Delete_Node->key==key && Delete_Node->previous == nullptr && Delete_Node->next != nullptr)
+			{
+				
+				Table[hash_index] = Delete_Node->next;
+				Delete_Node->next->previous = nullptr;
+				delete Delete_Node;
+				break;
+			}
+			//case 2 A Node in the middle of the list
+			else if (Delete_Node->key == key && Delete_Node->next!= nullptr && Delete_Node->previous!= nullptr)
+			{
+				//This reassigns our pointer values to reconnect our List and remove the Current Node found.
+				Delete_Node->next->previous = Delete_Node->previous;
+				Delete_Node->previous->next = Delete_Node->next;
+				delete Delete_Node;
+				break;
+			}
+			//Case 3 the end of the list
+			else if (Delete_Node->key == key && Delete_Node->next == nullptr && Delete_Node->previous != nullptr)
+			{
+				Delete_Node->previous->next = nullptr;
+				Delete_Node->previous = nullptr;
+				delete Delete_Node;
+				break;
+			}
+			//only one element in the list
+			else if (Delete_Node->key == key && Delete_Node->next == nullptr && Delete_Node->previous == nullptr)
+			{
+				delete Delete_Node;
+				Table[hash_index] = NULL;
+				break;
+			}
+			Delete_Node = Delete_Node->next;
+		}
+	}
+		
+
+
+	bool find(K key)
+	{
+		bool flag = false;
+		/*int hash_index = key % Table_Size;*/
+		//implementing the stl library hash function with K type value
+		std::hash<K> hash_function;
+		//declaring an integer value to store the hash value
+		int hash_index = hash_function(key);
+		//Modding the hash value by the table size to get a remainder for our index value
+		hash_index = std::abs(hash_index % Table_Size);
+		//creating a temporary NODE to be able to iterate through our linked list values
+		Hash_Node<K,V>* current = Table[hash_index];
+		//If the index of the hass is not NULL, begin loop
 		while (current != nullptr)
 		{
+			//compare the value of the current->key to the key value from our find function
 			if (current->key == key)
 			{
+				//standard print function to reveal the values of our list
 				std::cout << "Found:" << std::endl;
 				std::cout<< "Key: " << current->key << " " << "Value: " << current->value <<std::endl;
+				flag = true;
 			}
+			//this steps to the next value in our linked list
 			current = current->next;	
 		}
-		return NULL;
+		if (flag == false)
+		{
+			std::cout << "Key Value Not Found In Table!" << std::endl;
+		}
+		//had to modify to a bool function because strings cannot be NULL values.
+		return flag;
 			
 	}
 	void display()
 	{
+		//creating a display function to iterate through our list and test structural integrity
 		for (int i = 0; i < Table_Size; ++i)
 		{
+			//create a temporary node value that will allow our function to iterate through our linked list values
 			Hash_Node<K, V>* current = Table[i];
 			if (Table[i]==NULL)
 			{
+				//this prevents our function from crashing if there are index values with no elements stored in the list
 				continue;
 			}
+			//this allows our function to iterate through our linked list values stored in our hash table
 			while (current!=nullptr)
 			{
-				std::cout << "Index: " << i << " Key: " << current->key << " Value:" << current->value << std::endl;
+				std::cout << "Index: " << i << " Key: " << current->key << " Value:" << current->value <<std::endl;
+				if (current->previous!=nullptr)
+				{
+					std::cout << "Previous Key:" << current->previous->key << std::endl;
+				}
 				current = current->next;
 			}
 			//std::cout<<"Index: "<<i<<" Key: "<<Table[i]->key<<" Value:"<<Table[i]->value<<std::endl;
@@ -140,55 +210,8 @@ public:
 
 	~Hash_Table()
 	{
-		
+		delete[] Table;
 	}
 };
 
-//reference
-//https://medium.com/@aozturk/simple-hash-map-hash-table-implementation-in-c-931965904250
 #endif
-
-		/*
-
-
-				if (Table[hash_index] == NULL)
-		{
-			return Table[hash_index] = temp;
-		}
-					while (current != NULL)
-			{
-				previous = current;
-				current = current->next;
-				if (temp->key == current->key)
-				{
-					current->value = temp->value;
-				}
-				else
-				{
-					if (current->next == nullptr)
-					{
-						current->next = temp;
-					}
-					else
-					{
-						current = current->next;
-					}
-				}
-			}
-		*/
-		/*void Display()
-		{
-
-
-				if (Table[i] == NULL) continue;
-				Hash_Node<K, V>* temp = Table[i];
-
-				std::cout <<"Index: "<<i<<" Key: "<< temp->key <<" Value: "<< temp->value << std::endl;
-
-			/*
-			do
-			{
-				std::cout << "Index: " << i << " Key: " << temp->key << " Value: " << temp->value << std::endl;
-			} while (Table[i]->next != nullptr);
-		}
-		*/
--- Hash_Table/main.cpp
+++ Hash_Table/main.cpp
@@ -2,27 +2,125 @@
 #include "Hash_Tableh.h"
 #include <string>
 
+void automated_testing();
 
 int main()
 {
+	automated_testing();
+	Hash_Table<std::string, std::string> hash;
+	std::string key, value;
+	int choice;
+	bool exit = false;
+	while (1)
+	{
+		std::cout << "\n----------------------" << std::endl;
+		std::cout << "Hash Phone Directory" << std::endl;
+		std::cout << "\n----------------------" << std::endl;
+		std::cout << "1.Insert Phone Book Entry into the table" << std::endl;
+		std::cout << "2.Search Phone Book from the key" << std::endl;
+		std::cout << "3.Delete Phone Book Entry using Name" << std::endl;
+		std::cout << "4.Exit" << std::endl;
+		std::cout << "Enter your choice: ";
+		std::cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			std::cout << "Enter Phone Number to be inserted: ";
+			std::cin >> value;
+			std::cout << "Enter Last Name to be inserted: ";
+			std::cin >> key;
+			hash.insert(key, value);
+			break;
+		case 2:
+			std::cout << "Enter Last Name to be searched: ";
+			std::cin >> key;
+			std::cout << "/nLast Name found: " << key << std::endl;
+			if (hash.find(key) == false)
+			{
+				std::cout << "No Last Name found: " << key << std::endl;
+				continue;
+			}
+			break;
+		case 3:
+			std::cout << "Enter Last Name to be deleted: ";
+			std::cin >> key;
+			hash.Remove(key);
+			break;
+		case 4:
+			return 0;
+		default:
+			std::cout << "\nEnter correct option\n";
+		}
+	}
+
+
+
+	return 0;
+}
+
+
+
+
+void automated_testing()
+{
 	Hash_Table<int, int> Hash_Table;
-	Hash_Table.setTable_Size();
+
 	Hash_Table.insert(1, 1);
-	Hash_Table.display();
-	std::cout << std::endl;
+	if (Hash_Table.find(1) == false)
+	{
+		std::cout << "Insert Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Insert Value Test Successful!" << std::endl;
+	}
+
 	Hash_Table.insert(2, 2);
-	Hash_Table.display();
-	std::cout << std::endl;
+	if (Hash_Table.find(2) == false)
+	{
+		std::cout << "Insert Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Insert Value Test Successful!" << std::endl;
+	}
+
 	Hash_Table.insert(3, 3);
-	Hash_Table.display();
-	std::cout << std::endl;
+	if (Hash_Table.find(3) == false)
+	{
+		std::cout << "Insert Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Insert Value Test Successful!" << std::endl;
+	}
 	Hash_Table.insert(4, 4);
-	Hash_Table.display();
-	std::cout << std::endl;
+	if (Hash_Table.find(4) == false)
+	{
+		std::cout << "Insert Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Insert Value Test Successful!" << std::endl;
+	}
 	Hash_Table.insert(5, 5);
-	Hash_Table.display();
-	std::cout << std::endl;
+	if (Hash_Table.find(5) == false)
+	{
+		std::cout << "Insert Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Insert Value Test Successful!" << std::endl;
+	}
 	Hash_Table.insert(6, 2);
+	if (Hash_Table.find(6) == false)
+	{
+		std::cout << "Insert Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Insert Value Test Successful!" << std::endl;
+	}
 	Hash_Table.display();
 	std::cout << std::endl;
 	Hash_Table.insert(1, 3);
@@ -34,7 +132,45 @@ int main()
 	Hash_Table.find(3);
 	Hash_Table.find(2);
 	Hash_Table.find(1);
-	system("pause");
- 	std::cin.get();
-	return 0;
+	Hash_Table.Remove(2);
+	if (Hash_Table.find(2) == true)
+	{
+		std::cout << "Remove Value Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Remove Value Test Successful!" << std::endl;
+	}
+	Hash_Table.Remove(4);
+	if (Hash_Table.find(4) == true)
+	{
+		std::cout << "Remove Value Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Remove Value Test Successful!" << std::endl;
+	}
+	Hash_Table.Remove(1);
+	if (Hash_Table.find(1) == true)
+	{
+		std::cout << "Remove Value Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Remove Value Test Successful!" << std::endl;
+	}
+	Hash_Table.Remove(5);
+	if (Hash_Table.find(5) == true)
+	{
+		std::cout << "Remove Value Test Failed!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Remove Value Test Successful!" << std::endl;
+	}
+	Hash_Table.display();
+	std::cin.get();
 }
+
+
+
